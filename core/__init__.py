b6f8a66bdd4bb403f965b1d6ef37b25ab62d0106
jasonchanhk <chanjky97@gmail.com>
2022-09-07 17:51:58 +0100

    introduce the db back

--- core/__init__.py
+++ core/__init__.py
@@ -7,34 +7,34 @@ import json
 
 app = Flask(__name__)
 
-# conn_str = "mongodb+srv://josh:josh@cluster0.sl3j8fb.mongodb.net/?retryWrites=true&w=majority"
-# client = MongoClient(conn_str, tls=True, tlsAllowInvalidCertificates=True, serverSelectionTimeoutMS=5000)
+conn_str = "mongodb+srv://josh:josh@cluster0.sl3j8fb.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(conn_str, tls=True, tlsAllowInvalidCertificates=True, serverSelectionTimeoutMS=5000)
 
-# try:
-#     print(client.server_info())
-# except Exception:
-#     print("Unable to connect to the database server.")
+try:
+    print(client.server_info())
+except Exception:
+    print("Unable to connect to the database server.")
 
 CORS(app)
 
-# db = client["futureproof-test"]
-# test1 = db["test1"]
+db = client["futureproof-test"]
+test1 = db["test1"]
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-# @app.route("/cats", methods=["GET", "POST"])
-# def index():
-#     if request.method == "GET":
-#         all_cats = list(test1.find())   
-#         print(all_cats)
-#         # return json.loads(json_util.dumps(all_cats))
-#         return json_util.dumps(all_cats)
-#     elif request.method == "POST":
-#         data = request.json
-#         test1.insert_one(data)
-#         return f"{data['name']} has been created", 201
+@app.route("/cats", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        all_cats = list(test1.find())   
+        print(all_cats)
+        # return json.loads(json_util.dumps(all_cats))
+        return json_util.dumps(all_cats)
+    elif request.method == "POST":
+        data = request.json
+        test1.insert_one(data)
+        return f"{data['name']} has been created", 201
 
 # @app.route("/cats/<string:id>", )
 # def show(id):
